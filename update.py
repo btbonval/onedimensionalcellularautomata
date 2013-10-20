b19f38a05bebda2ce8a787bca29ad05e31bb82ee
Bryan Bonvallet <btbonval@gmail.com>
2013-10-20 13:14:36 -0400

    Improved loop detection with an option to run the automata until a loop is detected.

--- update.py
+++ update.py
@@ -14,6 +14,8 @@ from itertools import starmap
 from itertools import repeat 
 from itertools import izip
 
+from utils import bt2i
+
 def prepare_update(state, neighborhood, rule):
     '''
     state should be a 1D numpy array.
@@ -56,31 +58,52 @@ def update(*args, **kwargs):
 def evolve(steps, state, *args, **kwargs):
     '''
     Calls update() with steps iterations or until the state loops.
+    steps <= 0 will iterate until the state encounters a loop.
+    Plots a space-time graph using matplotlib.
     Returns a 2D array with increasing rows over time.
     '''
 
     l = len(state)
-    state_history = numpy.zeros( (steps, l), dtype=bool )
+    if steps <= 0:
+        # arbitrarily define a number of rows of history, a bit heuristic
+        state_history = numpy.zeros( (16*l,l), dtype=bool )
+        # ensure loop until repeat state if desired.
+        steps = numpy.inf
+    else:
+        # define worst case for history rows given a cut off point
+        state_history = numpy.zeros( (steps, l), dtype=bool )
  
-    # track all previous states
-    previous_states = set()
+    # track all previous states and which step they occurred
+    previous_states = {}
 
-    for s in range(0,steps):
+    s = 0
+    while s < (steps - 1):
         state_history[s,:] = state
-        previous_states.add(tuple(state_history[s,:]))
+        previous_states[bt2i(state_history[s,:])] = s
         state = update(state, *args, **kwargs)
-        # once stationary, always stationary. cut the execution
-        if (tuple(state) in previous_states): break
+        s = s + 1
+        # once a prediscovered state is found, nothing new can be discovered
+        if (bt2i(state) in previous_states):
+            break
+        if (s == state_history.shape[0]):
+            # double the state_history
+            state_history = numpy.concatenate( (state_history, numpy.zeros(state_history.shape)) )
+    # add that final state which broke execution
     state_history[s,:] = state
+    # switch to one past the last index for indexing arrays
     s = s + 1
 
     # plot the result
     pyplot.imshow(state_history[0:s,:], interpolation='none', cmap=cm.gray, aspect=l/float(s))
     pyplot.tick_params(axis='both', which='both', length=0, width=0, labelbottom='off', labeltop='off', labelleft='off', labelright='off')
+
+    # assume that once the given steps are reached, execution stopped forcibly.
+    # thus if the given steps are not reached, then execution stopped at a loop.
     if s == steps:
         pyplot.title('Truncated at ' + str(s) + ' steps.')
     else:
-        pyplot.title('Completed after ' + str(s) + ' steps.')
+        match_step = previous_states[bt2i(state)] + 1
+        pyplot.title('Completed after ' + str(s) + ' steps. Loops at step ' + str(match_step))
     pyplot.ylabel('< iterations')
     pyplot.xlabel('state')
     pyplot.show()
